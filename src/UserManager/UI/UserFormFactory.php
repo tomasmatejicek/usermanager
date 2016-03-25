5ba3ac55287cb0807cf256d6c2501df899d5b668
Josef Dohnal <josef.dohnal@mepatek.cz>
2016-03-25 15:40:25 +0100

    UI\UserFormFactory : add translator support, maseges move to public $messages array

--- src/UserManager/UI/UserFormFactory.php
+++ src/UserManager/UI/UserFormFactory.php
@@ -5,7 +5,9 @@ namespace Mepatek\UserManager\UI;
 use Nette\Application\UI\Form,
 	Nette\Object,
 	Nette\Security\User,
-	Nette\Security\AuthenticationException;
+	Nette\Utils\Validators,
+	Nette\Security\AuthenticationException,
+	Nette\Localization\ITranslator;
 
 /**
  * Class UserFormFactory
@@ -23,6 +25,13 @@ class UserFormFactory extends Object
 	 * @var integer
 	 */
 	public $passwordMinLevel = 2;
+
+	/**
+	 * Translator. If set all texts are translated
+	 * @var ITranslator
+	 */
+	public $translator = null;
+
 	/**
 	 * Event - login success
 	 * @var array
@@ -43,7 +52,35 @@ class UserFormFactory extends Object
 	 * @var array
 	 */
 	public $onRecoveryNotChangePassword;
+	/**
+	 * If do not use translator, can change forms and error messages
+	 *
+	 * @var array
+	 */
+	public $messages = [
+		"username"                    => "Username",
+		"username_required"           => "Please enter your username.",
+		"password"                    => "Password",
+		"password_required"           => "Please enter your password.",
+		"remember"                    => "Keep me signed in",
+		"signInSubmit"                => "Sign in",
+		"email"                       => "E-mail",
+		"email_required"              => "Please enter your e-mail.",
+		"forgotPasswordSubmit"        => "Send instruction for change password",
+		"newPassword"                 => "New password",
+		"newPassword_minLength"       => "Password length must least %d characters",
+		"newPassword_required"        => "Please enter password",
+		"newPasswordConfirm"          => "Confirm new password",
+		"newPasswordConfirm_notSame"  => "Password not same",
+		"newPasswordConfirm_required" => "Please enter password",
+		"recoveryPasswordSubmit"      => "Change password",
 
+		"err_username_or_password_incorrect" => "The username or password you entered is incorrect.",
+		"err_email_not_correct"              => "Please enter your e-mail.",
+		"err_email_does_not_exist"           => "E-mail does not exist.",
+		"err_password_too_simple"            => "Password is too simple, it should consist of large and small letters, numbers and special characters.",
+		"err_password_unable_to_change"      => "Unable to change password (token is expire)",
+	];
 	/** @var User (must set) */
 	protected $user;
 
@@ -59,18 +96,33 @@ class UserFormFactory extends Object
 	 */
 	public function createLoginForm()
 	{
-		$form = new Form();
-		$form->addText('username', 'Username:')
-			->setRequired('Please enter your username.');
+		$form = $this->getForm();
+		$form->addText('username', $this->messages['username'])
+			->setRequired($this->messages['username_required']);
 
-		$form->addPassword('password', 'Password:')
-			->setRequired('Please enter your password.');
+		$form->addPassword('password', $this->messages['password'])
+			->setRequired($this->messages['password_required']);
 
-		$form->addCheckbox('remember', 'Keep me signed in');
+		$form->addCheckbox('remember', $this->messages['remember']);
 
-		$form->addSubmit('send', 'Sign in');
+		$form->addSubmit('send', $this->messages['signInSubmit']);
 
 		$form->onSuccess[] = [$this, 'loginFormSucceeded'];
+		return $form;
+	}
+
+	/**
+	 * Get Form object
+	 * Set translator if is set
+	 * @return Form
+	 */
+	protected function getForm()
+	{
+		$form = new Form();
+		// set translator
+		if ($this->translator) {
+			$form->translator = $this->translator;
+		}
 		return $form;
 	}
 
@@ -95,11 +147,11 @@ class UserFormFactory extends Object
 		try {
 			$this->user->login($values->username, $values->password);
 		} catch (AuthenticationException $e) {
-			$form->addError('The username or password you entered is incorrect.');
+			$form->addError($this->messages['err_username_or_password_incorrect']);
 			return false;
 		}
 
-		$this->onLoginSuccess();
+		$this->onLoginSuccess($values);
 
 		return true;
 	}
@@ -114,11 +166,11 @@ class UserFormFactory extends Object
 	 */
 	public function createForgotPasswordForm()
 	{
-		$form = new Form();
-		$form->addText('email')
-			->setRequired('Please enter your e-mail.');
+		$form = $this->getForm();
+		$form->addText('email', $this->messages['email'])
+			->setRequired($this->messages['email_required']);
 
-		$form->addSubmit('send', 'Send instruction for change password');
+		$form->addSubmit('send', $this->messages['forgotPasswordSubmit']);
 
 		$form->onSuccess[] = [$this, 'forgotPasswordFormSucceeded'];
 		return $form;
@@ -136,15 +188,15 @@ class UserFormFactory extends Object
 	 */
 	public function forgotPasswordFormSucceeded(Form $form, $values)
 	{
-		// not set user
-		if (!$values->email) {
-			$form->addError('Please enter your e-mail.');
+		// not set email correct
+		if (!Validators::isEmail($values->email)) {
+			$form->addError($this->messages['err_email_not_correct']);
 			return false;
 		}
 
 		// if not token set, user with email not exist
 		if (!($token = $this->user->getAuthenticator()->resetPasswordToken($values->email))) {
-			$form->addError('E-mail does not exist.');
+			$form->addError($this->messages['err_email_does_not_exist']);
 			return false;
 		}
 
@@ -167,22 +219,20 @@ class UserFormFactory extends Object
 	 */
 	public function createRecoveryPasswordForm($token)
 	{
-		$form = new Form();
-
+		$form = $this->getForm();
 		$form->addHidden("token", $token);
 
-		$form->addPassword('password')
-			->addRule(Form::MIN_LENGTH, 'Password length must least %d characters', $this->passwordMinLength)
-			->setRequired('Please enter password');
-		$form->addPassword('passwordVerify', 'Heslo pro kontrolu:')
-			->setRequired('Please verify password')
-			->addRule(Form::EQUAL, 'Password not same', $form['password']);
-		$form->addSubmit('send', 'Change password');
+		$form->addPassword('newPassword', $this->messages['newPassword'])
+			->addRule(Form::MIN_LENGTH, $this->messages['newPassword_minLength'], $this->passwordMinLength)
+			->setRequired($this->messages['newPassword_required']);
+		$form->addPassword('newPasswordConfirm', $this->messages['newPasswordConfirm'])
+			->setRequired($this->messages['newPasswordConfirm_required'])
+			->addRule(Form::EQUAL, $this->messages['newPasswordConfirm_notSame'], $form['newPassword']);
+		$form->addSubmit('send', $this->messages['recoveryPasswordSubmit']);
 
 		$form->onSuccess[] = [$this, 'recoveryPasswordFormSucceeded'];
 		return $form;
 	}
-
 
 	/**
 	 * onSuccess event loginForm
@@ -198,24 +248,22 @@ class UserFormFactory extends Object
 	{
 
 		if (($passwordSafe = $this->user->getAuthenticator()->isPasswordSafe(
-				$values->password,
+				$values->newPassword,
 				$this->passwordMinLength,
 				$this->passwordMinLevel
 			)) > 0
 		) {
 			if ($passwordSafe == 2 or $passwordSafe == 6) {
-				$form->addError("Password length must least " . $this->passwordMinLength . " characters ");
+				$form->addError(sprintf($this->messages['newPassword_minLength'], $this->passwordMinLength));
 			}
 			if ($passwordSafe == 4 or $passwordSafe == 6) {
-				$form->addError(
-					"Password is too simple, it should consist of large and small letters, numbers and special characters."
-				);
+				$form->addError($this->messages['err_password_too_simple']);
 			}
 		}
 
 		if (!$this->user->getAuthenticator()->changePasswordToken($values->token, $values->password)) {
 			$this->onRecoveryNotChangePassword();
-			$form->addError('Unable to change password (token is expire)');
+			$form->addError($this->messages['err_password_unable_to_change']);
 			return false;
 		}
 
